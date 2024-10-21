e84b887da3757526036bf21e39338356aa493e52
Khoa Nguyen <n.d.khoa511@gmail.com>
2024-10-21 20:31:11 +0700

    setup bufferline

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -3,6 +3,8 @@ return {
   version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
+    require('bufferline').setup()
+    vim.opt.termguicolors = true
     vim.keymap.set('n', '<S-l>', '<Cmd>:BufferLineCycleNext<CR>')
     vim.keymap.set('n', '<S-h>', '<Cmd>:BufferLineCyclePrev<CR>')
   end,
