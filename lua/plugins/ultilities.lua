dae7ec57e1f0535724e956b7ac563fe5eded6448
Khoa Nguyen Dang <khoa.nguyendang@zoi.tech>
2025-08-12 16:26:55 +0700

    add nvim lint and mason

--- lua/plugins/ultilities.lua
+++ lua/plugins/ultilities.lua
@@ -11,6 +11,7 @@ vim.pack.add({
 	{ src = "https://github.com/akinsho/bufferline.nvim" },
 	{ src = "https://github.com/stevearc/conform.nvim" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/mfussenegger/nvim-lint" },
 })
 
 require("dressing").setup({})
@@ -57,6 +58,28 @@ require("gitsigns").setup({
 })
 
 require("mason").setup({})
+
+local lint = require("lint")
+
+lint.linters_by_ft = {
+	javascript = { "eslint_d" },
+	typescript = { "eslint_d" },
+	javascriptreact = { "eslint_d" },
+	typescriptreact = { "eslint_d" },
+}
+
+local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+	group = lint_augroup,
+	callback = function()
+		lint.try_lint()
+	end,
+})
+
+vim.keymap.set("n", "<leader>l", function()
+	lint.try_lint()
+end, { desc = "Trigger linting for current file" })
 
 local conform = require("conform")
 
