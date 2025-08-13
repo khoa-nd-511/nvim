2c60d1ee11a940ab71761c0b7d951fb5d1a472ee
Khoa Nguyen Dang <khoa.nguyendang@zoi.tech>
2025-08-13 15:57:26 +0700

    install copilot

--- lua/plugins/ultilities.lua
+++ lua/plugins/ultilities.lua
@@ -12,6 +12,7 @@ vim.pack.add({
 	{ src = "https://github.com/stevearc/conform.nvim" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/mfussenegger/nvim-lint" },
+	{ src = "https://github.com/github/copilot.vim" },
 })
 
 require("dressing").setup({})
